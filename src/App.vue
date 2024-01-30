ff676c8b153750cc873674373bca9306278a8442
xuyuliang <xyl@ode-tech.com>
2024-01-30 15:22:55 +0800

    大屏项目初始化上传

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-[url('assets/images/bg.png')] bg-cover bg-center h-screen text-white p-5 flex overflow-hidden">
+    <!-- 左 -->
+    <div></div>
+    <!-- 中 -->
+    <div></div>
+    <!-- 右 -->
+    <div></div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
